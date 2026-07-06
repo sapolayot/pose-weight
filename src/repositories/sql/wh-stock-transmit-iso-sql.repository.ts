fdbd8014aef4603b1a81d3b3c45d088c5934ac08
Sapolayot Nantawong <sapolayot.nif@gmail.com>
2026-07-06 15:43:19 +0700

    Format SQL

--- src/repositories/sql/wh-stock-transmit-iso-sql.repository.ts
+++ src/repositories/sql/wh-stock-transmit-iso-sql.repository.ts
@@ -1,7 +1,7 @@
 import { RowDataPacket } from "mysql2";
 import pool from "../../config/database.config";
-import WhStockTransmitIsoRepository from "../wh-stock-transmit-iso.repository";
 import type { WhStockTransmitIsoRecord } from "../../types/wh-stock-transmit-iso.type";
+import WhStockTransmitIsoRepository from "../wh-stock-transmit-iso.repository";
 
 interface WhStockTransmitIsoRow extends RowDataPacket {
   docNo: string;
@@ -20,45 +20,7 @@ interface WhStockTransmitIsoRow extends RowDataPacket {
   unitName: string;
 }
 
-const BASE_SQL = `
-  SELECT
-    wh_stock_transmit_iso.DocNo AS docNo,
-    DATE_FORMAT(wh_stock_transmit_iso.DocDate, '%d-%m-%Y') AS docDate,
-    wh_stock_transmit_iso.Detail AS detail,
-    wh_stock_transmit_iso.Status AS status,
-    wh_stock_transmit_iso.Manufacturing_Name AS manufacturingName,
-    wh_stock_transmit_iso.Manufacturing_Value AS manufacturingValue,
-    wh_stock_transmit_iso.Manufacturing_Unit AS manufacturingUnit,
-    wh_stock_transmit_iso.Manufacturing_LotNo AS manufacturingLotNo,
-    wh_stock_transmit_iso.Display_Status AS displayStatus,
-    DATE_FORMAT(wh_stock_transmit_iso.MFG_Date, '%d-%m-%Y') AS mfgDate,
-    DATE_FORMAT(wh_stock_transmit_iso.EXP_Date, '%d-%m-%Y') AS expDate,
-    DATE_FORMAT(wh_stock_transmit_iso.Print_Date, '%d-%m-%Y') AS printDate,
-    DATE_FORMAT(wh_stock_transmit_iso.Receive_Date, '%d-%m-%Y') AS receiveDate,
-    item_unit.Unit_Name AS unitName
-  FROM wh_stock_transmit_iso
-  LEFT JOIN item_unit
-    ON item_unit.Unit_Code = wh_stock_transmit_iso.Manufacturing_Unit
-  WHERE wh_stock_transmit_iso.Status = 0
-`;
-
-const SEARCH_SQL = `
-  AND (
-    wh_stock_transmit_iso.Manufacturing_LotNo LIKE ? OR
-    wh_stock_transmit_iso.Detail LIKE ? OR
-    wh_stock_transmit_iso.DocNo LIKE ? OR
-    wh_stock_transmit_iso.Manufacturing_Name LIKE ?
-  )
-`;
-
-const ORDER_SQL = `
-  ORDER BY wh_stock_transmit_iso.DocNo DESC
-  LIMIT ?
-`;
-
-export default class WhStockTransmitIsoSqlRepository
-  implements WhStockTransmitIsoRepository
-{
+export default class WhStockTransmitIsoSqlRepository implements WhStockTransmitIsoRepository {
   async searchActive(
     query = "",
     limit = 200,
@@ -67,15 +29,45 @@ export default class WhStockTransmitIsoSqlRepository
     const safeLimit = Math.min(Math.max(limit, 1), 200);
     const params: Array<string | number> = [];
 
-    let sql = BASE_SQL;
+    let sql = `
+      SELECT
+        wh_stock_transmit_iso.DocNo AS docNo,
+        DATE_FORMAT(wh_stock_transmit_iso.DocDate, '%d-%m-%Y') AS docDate,
+        wh_stock_transmit_iso.Detail AS detail,
+        wh_stock_transmit_iso.Status AS status,
+        wh_stock_transmit_iso.Manufacturing_Name AS manufacturingName,
+        wh_stock_transmit_iso.Manufacturing_Value AS manufacturingValue,
+        wh_stock_transmit_iso.Manufacturing_Unit AS manufacturingUnit,
+        wh_stock_transmit_iso.Manufacturing_LotNo AS manufacturingLotNo,
+        wh_stock_transmit_iso.Display_Status AS displayStatus,
+        DATE_FORMAT(wh_stock_transmit_iso.MFG_Date, '%d-%m-%Y') AS mfgDate,
+        DATE_FORMAT(wh_stock_transmit_iso.EXP_Date, '%d-%m-%Y') AS expDate,
+        DATE_FORMAT(wh_stock_transmit_iso.Print_Date, '%d-%m-%Y') AS printDate,
+        DATE_FORMAT(wh_stock_transmit_iso.Receive_Date, '%d-%m-%Y') AS receiveDate,
+        item_unit.Unit_Name AS unitName
+      FROM wh_stock_transmit_iso
+      LEFT JOIN item_unit
+        ON item_unit.Unit_Code = wh_stock_transmit_iso.Manufacturing_Unit
+      WHERE wh_stock_transmit_iso.Status = 0
+    `;
 
     if (trimmedQuery) {
-      sql += SEARCH_SQL;
+      sql += `
+        AND (
+          wh_stock_transmit_iso.Manufacturing_LotNo LIKE ? OR
+          wh_stock_transmit_iso.Detail LIKE ? OR
+          wh_stock_transmit_iso.DocNo LIKE ? OR
+          wh_stock_transmit_iso.Manufacturing_Name LIKE ?
+        )
+      `;
       const pattern = `%${trimmedQuery}%`;
       params.push(pattern, pattern, pattern, pattern);
     }
 
-    sql += ORDER_SQL;
+    sql += `
+      ORDER BY wh_stock_transmit_iso.DocNo DESC
+      LIMIT ?
+    `;
     params.push(safeLimit);
 
     const [rows] = await pool.query<WhStockTransmitIsoRow[]>(sql, params);
